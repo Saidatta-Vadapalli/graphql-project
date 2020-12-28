3b843a36fd1ebf95658c302f276a9bd8501a0d12
Saidatta.Vadapalli <hello@saidatta.dev>
2020-12-29 03:52:26 +0530

    made changes to app.js

--- server/App.js
+++ server/App.js
@@ -3,9 +3,12 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-  schema: schema;
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+  })
+);
 
 app.listen(4000, () => {
   console.log("Now listening for requests on port 4000");
